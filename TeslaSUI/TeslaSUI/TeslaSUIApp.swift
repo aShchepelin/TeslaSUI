c00c3e157b959950c37248505841f4e70446a14b
aShchepelin <aleksandr.shchepelin@ru.nestle.com>
2023-01-31 18:49:38 +0300

    initial commit

---
+++ TeslaSUI/TeslaSUI/TeslaSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaSUIApp.swift
+//  TeslaSUI
+//
+//  Created by Александр Андреевич Щепелин on 31.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TeslaSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
